403252b29886e5929ccdb483f7be8ae696e9c798
timjklein36 <timjklein36@gmail.com>
2017-12-06 14:54:41 -0500

    Clean up some of the processing

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ if(arglength > 2) {
   if(process.argv[2] === '-n') {
     if(arglength >= 4) {
       numRequests = +process.argv[3];
-      console.log(numRequests);
     } else {
       console.error("Please supply an integer number of requests to process.");
     }
@@ -18,25 +17,32 @@ if(arglength > 2) {
 
 if(numRequests > 0) {
   var lamOh = [], lamVa = [];
-  console.log("=========== - Creating Requests - ===========");
+  console.log("=========== - Creating", numRequests, "Requests - ===========");
   for(let i = 0; i < numRequests; i++) {
-    // lamVa.push(new AWS.Lambda({apiVersion: '2015-03-31', region: 'us-east-1'})
-    //   .invoke({FunctionName:'<FIND NAME>',Payload:'{"regions":["us-east-1"]}'})
-    //   .promise());
+    lamVa.push(new AWS.Lambda({apiVersion: '2015-03-31', region: 'us-east-1'})
+      .invoke({FunctionName:'ReportTool',Payload:'["us-east-1"]'})
+      .promise());
     lamOh.push(new AWS.Lambda({apiVersion: '2015-03-31', region: 'us-east-2'})
       .invoke({FunctionName:'getAllEC2',Payload:'{"regions":["us-east-1"]}'})
       .promise());
   }
+  console.log("=========== - Requests Created - ===========");
 
-  for(let promise of lamOh) {
+  console.log("=========== - Processing Requests - ===========");
+  processPromises(lamOh);
+  processPromises(lamVa);
+
+}
+
+function processPromises(promises) {
+  for(let promise of promises) {
     promise.then(
       (data) => {
-        console.log(data);
+        console.log(data.Payload);
       },
       (err) => {
         console.error(err, err.stack);
       }
     );
   }
-
 }
